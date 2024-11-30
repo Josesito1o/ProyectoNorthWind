0c868e72525f838f5cefb5079e2b4fa33a2dbb06
Alejandro Parra <josesito10237111@gmail.com>
2024-11-30 01:50:27 -0500

    Abstracciones con dependencias y AppLogger

---
+++ NorthWind.ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using NorthWind.ConsoleApp.Services;
+using NorthWind.Entities.Interfaces;
+using NorthWind.Writers;
+
+IUserActionWriter Writer = new ConsoleWriter();
+
+AppLogger Logger = new AppLogger(Writer);
+Logger.WriteLog("Aplication Started.");
+
+ProductService Service = new ProductService(Writer);
+Service.Add("Demo", "Azucar refinada");
